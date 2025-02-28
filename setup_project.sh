432304fe9fd2907a367e9e0b58660a44683db925
tobito8897 <sergiosanchezhernandez80@gmail.com>
2025-02-27 23:29:35 -0600

    Update setup script to create directories

--- setup_project.sh
+++ setup_project.sh
@@ -1,19 +1,6 @@
 #! /bin/bash
 set -e
 
-########### ENVIRONMENT VARIABLES ##############
-### Set before running the rest of the setup ###
-BIOMARKERS_PROJECT_HOME=/mnt/c/Users/tobit/Documents/Doctorado/Initiative1_epic1/Project
-CHB_DATASET_HOME=/mnt/c/Users/tobit/Downloads/eeg_datasets/chb-mit
-SIENA_DATASET_HOME=/mnt/c/Users/tobit/Downloads/eeg_datasets/siena
-TUSZ_DATASET_HOME=/mnt/c/Users/tobit/Downloads/eeg_datasets/tusz
-
-grep -qF -- "BIOMARKERS_PROJECT_HOME" ~/.bashrc || echo -e "\nexport BIOMARKERS_PROJECT_HOME=${BIOMARKERS_PROJECT_HOME}" >> ~/.bashrc
-grep -qF -- "CHB_DATASET_HOME" ~/.bashrc || echo -e "export CHB_DATASET_HOME=${CHB_DATASET_HOME}" >> ~/.bashrc
-grep -qF -- "TUSZ_DATASET_HOME" ~/.bashrc || echo -e "export SIENA_DATASET_HOME=${SIENA_DATASET_HOME}" >> ~/.bashrc
-grep -qF -- "TUSZ_DATASET_HOME" ~/.bashrc || echo -e "export TUSZ_DATASET_HOME=${TUSZ_DATASET_HOME}" >> ~/.bashrc
-source ~/.bashrc
-
 # Install ubuntu packages for building python
 sudo apt install -y make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
@@ -39,3 +26,20 @@ python -m pip install -t requirements.txt
 deactivate
 
 # Create directory tree
+BIOMARKERS_PROJECT_HOME="$(head -1 .env | cut -d "=" -f 2)"
+mkdir ${BIOMARKERS_PROJECT_HOME}/data
+mkdir ${BIOMARKERS_PROJECT_HOME}/docs
+mkdir ${BIOMARKERS_PROJECT_HOME}/images
+mkdir ${BIOMARKERS_PROJECT_HOME}/slices/
+mkdir ${BIOMARKERS_PROJECT_HOME}/slices/chb-mit
+mkdir ${BIOMARKERS_PROJECT_HOME}/slices/siena
+mkdir ${BIOMARKERS_PROJECT_HOME}/slices/tusz
+mkdir ${BIOMARKERS_PROJECT_HOME}/windows/
+mkdir ${BIOMARKERS_PROJECT_HOME}/windows/chb-mit
+mkdir ${BIOMARKERS_PROJECT_HOME}/windows/siena
+mkdir ${BIOMARKERS_PROJECT_HOME}/windows/tusz
+mkdir ${BIOMARKERS_PROJECT_HOME}/reports/
+mkdir ${BIOMARKERS_PROJECT_HOME}/features/
+mkdir ${BIOMARKERS_PROJECT_HOME}/features/chb-mit
+mkdir ${BIOMARKERS_PROJECT_HOME}/features/siena
+mkdir ${BIOMARKERS_PROJECT_HOME}/features/tusz
