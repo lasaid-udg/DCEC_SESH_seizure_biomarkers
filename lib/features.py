c9fde167c388706353427dc201538ce8fa77e71b
tobito8897 <sergiosanchezhernandez80@gmail.com>
2025-03-13 19:48:23 -0600

    Add new univariate features

--- lib/features.py
+++ lib/features.py
@@ -1,7 +1,7 @@
-import os
 import numpy
 import pandas
-import scipy.stats 
+import scipy.stats
+import mne_features.univariate 
 
 
 class UnivariateFeatureGateway():
@@ -19,3 +19,13 @@ class UnivariateFeatureGateway():
 
     def kurtosis(self, data: numpy.array) -> float:
         return numpy.round(scipy.stats.kurtosis(data), decimals=5)
+
+    def hjorth_mobility(self, data: numpy.array) -> float:
+        return numpy.round(mne_features.univariate.compute_hjorth_mobility(data), decimals=5)
+
+    def hjorth_complexity(self, data: numpy.array) -> float:
+        return numpy.round(mne_features.univariate.compute_hjorth_complexity(data), decimals=5)
+
+    def katz_fractal_dimension(self, data: numpy.array) -> float:
+        data = numpy.expand_dims(data, axis=0)
+        return numpy.round(mne_features.univariate.compute_katz_fd(data)[0], decimals=5)
